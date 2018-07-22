623f22ab42bd08f67b650d40831e2352dc4e3eab
Bycor Sánchez <bycor1989@gmail.com>
2018-07-22 18:35:22 +0100

    feat: add shelf selection
    Add shelf selection in Book component

--- src/Book.js
+++ src/Book.js
@@ -1,21 +1,39 @@
 import React, { Component } from "react";
 import PropType from 'prop-types';
+import { shelfTypes } from "./BooksAPI";
 
-class Book extends Component{
+class Book extends Component {
     static propTypes = {
         book: PropType.object.isRequired
     };
 
-    render(){
+    render() {
         const { book } = this.props;
 
         return (
             <div className="book">
                 <div className="book-top">
-                    <img src={book.imageLinks.thumbnail} alt={book.title} className="book-cover"/>
+                    <img src={book.imageLinks.thumbnail} alt={book.title} className="book-cover" />
+                    <div className="book-shelf-changer">
+                        <select defaultValue={book.shelf}>
+                            <option value="move" disabled>Move to...</option>
+                            {   //Create options according to shelves
+                                shelfTypes.map(({ key, label }) => (
+                                    <option
+                                        key={key}
+                                        value={key}
+                                    >{label}</option>
+                                ))
+                            }
+                            <option value="none">None</option>
+                        </select>
+                    </div>
                 </div>
                 <h3 className="book-title">{book.title}</h3>
-                { book.authors.map(author => (<p key={author} className="book-authors">{author}</p>))}
+
+                { // Display each author in a new line
+                    book.authors.map(author => (<p key={author} className="book-authors">{author}</p>))
+                }
             </div>
         );
     }
